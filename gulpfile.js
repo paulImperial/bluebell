d1cb440849859940454b207d2f2fa40f227efc6b
Paul Mason <paul.mason@experian.com>
2017-03-03 21:17:55 +0000

    updte header and footer - add blog

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,20 @@ var mustache = require("gulp-mustache-plus");
 var cleanCSS = require("gulp-clean-css");
 var browserSync = require('browser-sync');
 var imagemin = require('gulp-imagemin');
+var del = require('del');
+
+gulp.task('clean', function() {
+    del('dist/css');
+});
 
 gulp.task('mustache', function(){
     gulp.src("./templates/*.mustache")
-        .pipe (mustache({},{},{
+        .pipe (mustache({
+            msg:{
+                text:'bob',
+                date:'now'
+            }
+        },{},{
             head: "./templates/partials/head.mustache",
             header: "./templates/partials/header.mustache",
             footer: "./templates/partials/footer.mustache",
