f82e90900b9628f175592d285f00b51e44d611d8
Paul Mason <paul.imperialroad@gmail.com>
2022-02-22 21:11:59 +0000

    add dist folder

---
+++ dist/js/script.js
@@ -0,0 +1,49 @@
+$("#nav-icon1").click(function() {
+  $(this).toggleClass("open");
+  $("nav").toggleClass("displayBlock");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  //init();
+
+  const blogWrapper = document.querySelectorAll(".blog-wrapper");
+
+  blogWrapper.forEach(blog => {
+    blog.addEventListener("click", function() {
+      // blogWrapper.forEach(blogwrap => {
+      // 	blogwrap.querySelector('.blog-blurb').classList.add('line-clamp')
+      // })
+      blog.querySelector(".blog-blurb").classList.toggle("line-clamp");
+    });
+  });
+});
+
+function init() {
+  // set cookie for cookie policy
+
+  const cookieBanner = document.querySelector("#js-cookie");
+
+  const checkCookieValue = () => {
+    if (
+      document.cookie
+        .split(";")
+        .filter(item => item.trim().startsWith("cookieAgree=")).length
+    ) {
+      // do not show cookie
+      console.log("no need to do anything");
+    } else {
+      // show cookie
+      console.log("show cookie banner");
+      cookieBanner.classList.add("show-cookie-banner");
+    }
+  };
+
+  const acceptButton = document.querySelector("#js-cookie_accept");
+
+  acceptButton.addEventListener("click", function() {
+    document.cookie = "cookieAgree=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    cookieBanner.classList.remove("show-cookie-banner");
+  });
+
+  checkCookieValue();
+}
